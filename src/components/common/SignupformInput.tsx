1c2794000a122080b8e3653429929bf07805393f
heejung-newheee <aldls2311@gmail.com>
2024-03-05 18:38:28 +0900

    chore: ui 설치

--- src/components/common/SignupformInput.tsx
+++ src/components/common/SignupformInput.tsx
@@ -1,35 +1,44 @@
-'use client';
-
-import { SubmitHandler, useForm } from 'react-hook-form';
-
-type Inputs = {
-    example: string;
-    exampleRequired: string;
-};
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 
 export default function SignupFormInput() {
-    const {
-        register,
-        handleSubmit,
-        watch,
-        formState: { errors }
-    } = useForm<Inputs>();
-    const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
-
-    console.log(watch('example')); // watch input value by passing the name of it
-
     return (
-        /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
-        <form onSubmit={handleSubmit(onSubmit)}>
-            {/* register your input into the hook by invoking the "register" function */}
-            <input defaultValue="test" {...register('example')} />
-
-            {/* include validation with required or other standard HTML validation rules */}
-            <input {...register('exampleRequired', { required: true })} />
-            {/* errors will return when field validation fails  */}
-            {errors.exampleRequired && <span>This field is required</span>}
-
-            <input type="submit" />
-        </form>
+        <Card className="w-[350px]">
+            <CardHeader>
+                <CardTitle>Create project</CardTitle>
+                <CardDescription>Deploy your new project in one-click.</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <form>
+                    <div className="grid w-full items-center gap-4">
+                        <div className="flex flex-col space-y-1.5">
+                            <Label htmlFor="name">Name</Label>
+                            <Input id="name" placeholder="Name of your project" />
+                        </div>
+                        <div className="flex flex-col space-y-1.5">
+                            <Label htmlFor="framework">Framework</Label>
+                            <Select>
+                                <SelectTrigger id="framework">
+                                    <SelectValue placeholder="Select" />
+                                </SelectTrigger>
+                                <SelectContent position="popper">
+                                    <SelectItem value="next">Next.js</SelectItem>
+                                    <SelectItem value="sveltekit">SvelteKit</SelectItem>
+                                    <SelectItem value="astro">Astro</SelectItem>
+                                    <SelectItem value="nuxt">Nuxt.js</SelectItem>
+                                </SelectContent>
+                            </Select>
+                        </div>
+                    </div>
+                </form>
+            </CardContent>
+            <CardFooter className="flex justify-between">
+                <Button variant="outline">Cancel</Button>
+                <Button>Deploy</Button>
+            </CardFooter>
+        </Card>
     );
 }
