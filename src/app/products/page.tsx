840d387fcf59ecb54d14232b3b6fe0839d61ae14
heejung-newheee <aldls2311@gmail.com>
2024-03-15 11:20:14 +0900

    Rename:폴더구조 변경 전체 레이아웃(헤더/푸터 추가)과 페이지별 구성

---
+++ src/app/products/page.tsx
@@ -0,0 +1,7 @@
+export default function page() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div>제품목록 페이지</div>
+        </main>
+    );
+}
