3517f17cbd1fce75011c9519499ad47233fb7afc
heejung-newheee <aldls2311@gmail.com>
2024-03-15 10:49:17 +0900

    Rename:폴더구조 변경 전체 레이아웃과 페이지별로 구성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import { ThemeProvider } from '@/components/ThemeProvider';
-import ModeToggle from '@/components/common/ModeToggle';
+import Footer from '@/components/footer/Footer';
+import ModeToggle from '@/components/header/ModeToggle';
+import Nav from '@/components/header/Nav';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
@@ -7,7 +9,7 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-    title: 'Sign-up/Sign-In',
+    title: 'e-commers-prac',
     description: '간단한 회원가입/로그인 페이지'
 };
 
@@ -20,9 +22,11 @@ export default function RootLayout({
         <html lang="en">
             <body className={inter.className}>
                 <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+                    <Nav />
                     <ModeToggle />
                     {children}
                     {/* <Toaster /> */}
+                    <Footer />
                 </ThemeProvider>
             </body>
         </html>
--- src/app/signIn/page.tsx
+++ src/app/signIn/page.tsx
@@ -1,7 +1,7 @@
-export default function Home() {
+export default function page() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <div>메인입니다</div>
+            <div>로그인 페이지 입니다</div>
         </main>
     );
 }
