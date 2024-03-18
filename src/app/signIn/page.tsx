a30d5cdd0e47f2465427c3951474f3b43cc07f58
heejung-newheee <aldls2311@gmail.com>
2024-03-19 01:28:41 +0900

    feat: 로그인 페이지 shadcn card ui 적용 react hook form 적용

--- src/app/signIn/page.tsx
+++ src/app/signIn/page.tsx
@@ -1,7 +1,54 @@
-export default function page() {
+'use client';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { useForm } from 'react-hook-form';
+
+export default function SignIn() {
+    const {
+        register,
+        handleSubmit,
+        formState: { isSubmitting }
+    } = useForm();
+
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <div>로그인 페이지 입니다</div>
+            <Card className="w-[350px]">
+                <CardHeader>
+                    <CardTitle>로그인</CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <form
+                        onSubmit={handleSubmit(async (data) => {
+                            await new Promise((r) => setTimeout(r, 1000));
+                            alert(JSON.stringify(data));
+                        })}>
+                        <div className="space-y-1.5">
+                            <div className="grid w-full items-center gap-4">
+                                <div className="flex flex-col space-y-1.5">
+                                    <Label htmlFor="email">이메일</Label>
+                                    <Input id="email" type="email" placeholder="example@example.com" {...register('email')} />
+                                </div>
+                            </div>
+                            <div className="grid w-full items-center gap-4">
+                                <div className="flex flex-col space-y-1.5">
+                                    <Label htmlFor="email">비밀번호</Label>
+                                    <Input id="password" type="password" placeholder="******" {...register('password')} />
+                                </div>
+                            </div>
+                        </div>
+                        <br />
+                        <div className="flex justify-between">
+                            <Button variant="ghost">회원가입</Button>
+                            <Button type="submit" disabled={isSubmitting}>
+                                로그인
+                            </Button>
+                        </div>
+                    </form>
+                </CardContent>
+            </Card>
         </main>
     );
 }
--- src/app/signUp/page.tsx
+++ src/app/signUp/page.tsx
@@ -1,6 +1,6 @@
 import SignUpForm from '@/components/auth/SignUpForm';
 
-export default function page() {
+export default function SignUp() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <SignUpForm />
