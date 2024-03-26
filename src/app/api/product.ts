f7568337440a89dfe90f4ee34cccd4b4a426de35
heejung-newheee <aldls2311@gmail.com>
2024-03-26 13:51:50 +0900

    Feat: 상품페이지 상품목록, 상품 상세페이지

---
+++ src/app/api/product.ts
@@ -0,0 +1,8 @@
+import { supabase } from '@/lib/supabase/supabase';
+
+export const productInfo = async (id: string) => {
+    const { data, error } = await supabase.from('products').select().eq('id', id).single();
+    if (error) throw error;
+    console.log(data);
+    return data;
+};
--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,8 +1,73 @@
+'use client';
+import { productInfo } from '@/app/api/product';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent } from '@/components/ui/card';
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel';
+
+import { ProductType } from '@/lib/supabase/database.types';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+
 export default function ProductDetail(props: any) {
+    const [productData, setProductData] = useState<ProductType>();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const data = await productInfo(props.params.id);
+                setProductData(data);
+            } catch (error) {
+                console.error('Error fetching product info:', error);
+            }
+        };
+        fetchData();
+    }, [props.params.id]); // props.params.id가 변경될 때마다 다시 호출
+
+    console.log(productData);
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <div>제품 상세 페이지</div>
-            parameters : {props.params.id}
+            {productData ? (
+                <div className="flex flex-wrap">
+                    <Carousel className="max-w-96 mx-20 mb-20">
+                        <CarouselContent>
+                            {productData.images &&
+                                productData.images.map((_, index) => (
+                                    <CarouselItem key={productData?.id}>
+                                        <Card>
+                                            <CardContent className="flex aspect-square items-center justify-center p-0 overflow-hidden">
+                                                {productData.images ? (
+                                                    <Image
+                                                        src={productData.images[index]}
+                                                        alt=""
+                                                        height={900}
+                                                        width={900}
+                                                        layout="responsive"
+                                                    />
+                                                ) : null}
+                                            </CardContent>
+                                        </Card>
+                                    </CarouselItem>
+                                ))}
+                        </CarouselContent>
+                        <CarouselPrevious />
+                        <CarouselNext />
+                    </Carousel>
+                    <div className="">
+                        <div className="">
+                            <p className="text-xs text-gray-300">{productData.category}</p>
+                            <h3 className="text-lg font-bold">{productData.product_name}</h3>
+                            <p>{productData.description}</p>
+                            <p className="text-lg font-bold">{productData.price}</p>
+                        </div>
+                        <div className="flex pt-3 gap-2">
+                            <Button variant="outline">장바구니</Button>
+                            <Button>구매하기</Button>
+                        </div>
+                    </div>
+                </div>
+            ) : (
+                <p>상품페이지 중비중입니다</p>
+            )}
         </main>
     );
 }
